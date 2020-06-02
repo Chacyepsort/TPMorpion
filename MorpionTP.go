0425a689e82713b6dfca8946c3d052b9cc28a1c7
Chacyesport <56914210+Chacyepsort@users.noreply.github.com>
2020-06-02 14:09:03 +0200

    Add files via upload

--- MorpionTP.go
+++ MorpionTP.go
@@ -6,6 +6,7 @@ import (
 
 var nbCoups int = 0
 var tableauMorpion = [9]int{0,0,0,0,0,0,0,0,0}
+var gagner = false
 
 
 func main() {
@@ -26,44 +27,44 @@ func wndOnClose(arg *winc.Event) {
     winc.Exit()
 }
 
-func CreateButton(posX int, posY int, window *winc.Form) {
+func CreateButton(position, posX int, posY int, window *winc.Form) {
     btn := winc.NewPushButton(window)
     btn.SetPos(posX, posY)
     btn.SetSize(60, 60)
     btn.SetText("")
     btn.OnClick().Bind(func(e *winc.Event){
-        ButtonClicked(btn, window)
+        if (tableauMorpion[position] == 0) {
+            if (gagner == false) {
+                if (nbCoups >= 9){
+                    DisplayInformations("Tous les coups ont été joué, pas de gagnants", window)
+                } else {
+                    if (nbCoups % 2 == 0) {
+                        btn.SetText("X")
+                        tableauMorpion[position] = 1
+                    } else {
+                        btn.SetText("O")
+                        tableauMorpion[position] = 2
+                    }
+                    nbCoups++
+                    VerifGagner(window)
+                }
+            }
+        }
     })
 }
 
 func DisplayButton(window *winc.Form){
-    CreateButton(100,100,window)
-    CreateButton(170,100,window)
-    CreateButton(240,100,window)
-    CreateButton(100,170,window)
-    CreateButton(170,170,window)
-    CreateButton(240,170,window)
-    CreateButton(100,240,window)
-    CreateButton(170,240,window)
-    CreateButton(240,240,window)
+    CreateButton(0,100,100,window)
+    CreateButton(1,170,100,window)
+    CreateButton(2,240,100,window)
+    CreateButton(3,100,170,window)
+    CreateButton(4,170,170,window)
+    CreateButton(5,240,170,window)
+    CreateButton(6,100,240,window)
+    CreateButton(7,170,240,window)
+    CreateButton(8,240,240,window)
 }
 
-func ButtonClicked(button *winc.PushButton, window *winc.Form) {
-    if (nbCoups >= 9){
-        DisplayInformations("Tous les coups ont été joué, pas de gagnants", window)
-    } else {
-        if (nbCoups % 2 == 0) {
-            button.SetText("X")
-            tableauMorpion[nbCoups] = 1
-        } else {
-            button.SetText("O")
-            tableauMorpion[nbCoups] = 2
-        }
-        VerifGagner(window)
-        nbCoups++
-    }
-
-}
 
 func DisplayInformations(text string,window *winc.Form){
     textInformations :=winc.NewLabel(window)
@@ -82,6 +83,7 @@ func VerifGagner(window *winc.Form){
         tableauMorpion[0] == 1 && tableauMorpion[4] == 1 && tableauMorpion[8] == 1 ||
         tableauMorpion[2] == 1 && tableauMorpion[4] == 1 && tableauMorpion[6] == 1) {
         DisplayInformations("Le joueur 1 à gagner",window)
+        gagner = true
       } else if (tableauMorpion[0] == 2 && tableauMorpion[1] == 2 && tableauMorpion[2] == 2 ||
         tableauMorpion[3] == 2 && tableauMorpion[4] == 2 && tableauMorpion[5] == 2 ||
         tableauMorpion[6] == 2 && tableauMorpion[7] == 2 && tableauMorpion[8] == 2 || 
@@ -91,10 +93,11 @@ func VerifGagner(window *winc.Form){
         tableauMorpion[0] == 2 && tableauMorpion[4] == 2 && tableauMorpion[8] == 2 ||
         tableauMorpion[2] == 2 && tableauMorpion[4] == 2 && tableauMorpion[6] == 2) {
         DisplayInformations("Le joueur 2 à gagner",window)
+        gagner = true
       } else {
         DisplayInformations("Le joueur " + strconv.FormatInt(int64(nbCoups % 2 + 1), 10) + " doit jouer.",window)
-        DisplayInformations(strconv.FormatInt(int64(tableauMorpion[0]), 10) + strconv.FormatInt(int64(tableauMorpion[1]), 10) +strconv.FormatInt(int64(tableauMorpion[2]), 10) , window)
       }
+
 }
 
 
